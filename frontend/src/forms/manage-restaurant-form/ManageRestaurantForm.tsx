3d57fb3e1f2d3887cf14dbd764d08d9226f4e0d4
Praveen Kumar <praveen9166050@gmail.com>
2024-10-11 13:54:33 +0530

    ManageRestaurantPage, ManageRestaurantForm (details section)

---
+++ frontend/src/forms/manage-restaurant-form/ManageRestaurantForm.tsx
@@ -0,0 +1,58 @@
+import { Form } from "@/components/ui/form";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import DetailsSection from "./DetailsSection";
+
+const formSchema = z.object({
+  restaurantName: z.string({required_error: "Restaurant name is required"}),
+  city: z.string({required_error: "City is required"}),
+  country: z.string({required_error: "Country is required"}),
+  deliveryPrice: z.coerce.number({
+    required_error: "Delivery price is required",
+    invalid_type_error: "Derivery price must be a number"
+  }),
+  estimatedDeliveryTime: z.coerce.number({
+    required_error: "Estimated delivery time is required",
+    invalid_type_error: "Estimated derivery time must be a number"
+  }),
+  cuisines: z.array(z.string()).nonempty({message: "Please select atleast one item"}),
+  menuItems: z.array(z.object({
+    name: z.string({required_error: "Name is required"}),
+    price: z.coerce.number({
+      required_error: "Price is required",
+      invalid_type_error: "Price must be a number"
+    })
+  })),
+  imageFile: z.instanceof(File, {message: "Image is required"})
+});
+
+type Restaurantformdata = z.infer<typeof formSchema>;
+
+type Props = {
+  onSave: (restaurantFormdata: FormData) => void,
+  isLoading: boolean
+}
+
+function ManageRestaurantForm({onSave, isLoading}: Props) {
+  const form = useForm<Restaurantformdata>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      cuisines: [],
+      menuItems: [{name: "", price: 0}]
+    }
+  });
+
+  const onSubmit = (formDataJson: Restaurantformdata) => {
+
+  }
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8 bg-gray-50 p-10 rounded-lg">
+        <DetailsSection />
+      </form>
+    </Form>
+  );
+}
+
+export default ManageRestaurantForm;
