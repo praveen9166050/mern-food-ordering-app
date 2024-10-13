91981a9a12454168083b8a147ca487ceb81ba554
Praveen Kumar <praveen9166050@gmail.com>
2024-10-13 15:35:36 +0530

    SearchPage layout with result cards

---
+++ frontend/src/components/SearchResultInfo.tsx
@@ -0,0 +1,22 @@
+import { Link } from "react-router-dom";
+
+type Props = {
+  total: number,
+  city: string
+}
+
+function SearchResultInfo({total, city}: Props) {
+  return (
+    <div className="flex flex-col gap-3 text-xl font-bold justify-between lg:items-center lg:flex-row">
+      <span>
+        {total} Restaurants found in {city}
+        <Link to={"/"} className="text-sm font-semibold underline ml-1 cursor-pointer text-blue-500">
+          Change location
+        </Link>
+      </span>
+      insert sort dropdown here
+    </div>
+  );
+}
+
+export default SearchResultInfo;
--- frontend/src/pages/SearchPage.tsx
+++ frontend/src/pages/SearchPage.tsx
@@ -1,21 +1,42 @@
 import { useSearchRestaurant } from "@/api/RestaurantApi";
+import SearchResultCard from "@/components/SearchResultCard";
+import SearchResultInfo from "@/components/SearchResultInfo";
 import { useParams } from "react-router-dom";
 
 function SearchPage() {
   const {city} = useParams();
   const {results, isLoading} = useSearchRestaurant(city);
+  if (isLoading) {
+    return "Loading...";
+  }
+  if (!results?.data || !city) {
+    return <span>No results found</span>
+  }
   return (
-    <span>
-      User searched for {city}{" "}
-      <span>
-        {results?.data.map(restaurant => (
-          <span>
-            found - {restaurant.restaurantName}, {restaurant.city}
-          </span>
+    <div className="grid grid-cols-1 lg:grid-cols-[250px_1fr] gap-5">
+      <div id="cuisines-list">
+        insert cuisines here :)
+      </div>
+      <div id="main-content" className="flex flex-col gap-5">
+        <SearchResultInfo total={results.pagination.total} city={city} />
+        {results.data.map((restaurant) => (
+          <SearchResultCard restaurant={restaurant} />
         ))}
-      </span>
-    </span>
-  )
+      </div>
+    </div>
+  );
+  // return (
+  //   <span>
+  //     User searched for {city}{" "}
+  //     <span>
+  //       {results?.data.map(restaurant => (
+  //         <span>
+  //           found - {restaurant.restaurantName}, {restaurant.city}
+  //         </span>
+  //       ))}
+  //     </span>
+  //   </span>
+  // )
 }
 
 export default SearchPage;
