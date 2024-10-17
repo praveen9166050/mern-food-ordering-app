63c1599e673c35902248fb86aaf2ceb61f00b07c
Praveen Kumar <praveen9166050@gmail.com>
2024-10-17 23:06:19 +0530

    Create checkout session API request

--- frontend/src/types.ts
+++ frontend/src/types.ts
@@ -44,3 +44,19 @@ export type SearchRestaurantResponse = {
     pages: number
   }
 }
+
+export type CheckoutSessionRequest = {
+  cartItems: {
+    menuItemId: string,
+    name: string,
+    quantity: string
+  }[],
+  deliveryDetails: {
+    email: string,
+    name: string,
+    addressLine1: string,
+    city: string,
+    country: string
+  },
+  restaurantId: string
+}
