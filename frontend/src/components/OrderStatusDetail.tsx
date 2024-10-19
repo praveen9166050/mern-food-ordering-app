e130612fcc59ce8cb1c1b1ae0f6e1639fbf78538
Praveen Kumar <praveen9166050@gmail.com>
2024-10-19 22:12:57 +0530

    Order status detail

---
+++ frontend/src/components/OrderStatusDetail.tsx
@@ -0,0 +1,33 @@
+import { Order } from "@/types"
+import { Separator } from "./ui/separator";
+
+type Props = {
+  order: Order
+}
+
+function OrderStatusDetail({order}: Props) {
+  return (
+    <div className="space-y-5">
+      <div className="flex flex-col">
+        <span className="font-bold">Delivering to:</span>
+        <span>{order.deliveryDetails.name}</span>
+        <span>{order.deliveryDetails.addressLine1}, {order.deliveryDetails.city}</span>
+      </div>
+      <div className="flex flex-col">
+        <span className="font-bold">Your order</span>
+        <ul>
+          {order.cartItems.map(cartItem => (
+            <li>{cartItem.name} x {cartItem.quantity}</li>
+          ))}
+        </ul>
+      </div>
+      <Separator />
+      <div className="flex flex-col">
+        <span className="font-bold">Total</span>
+        <span>&#8377; {order.totalAmount}</span>
+      </div>
+    </div>
+  );
+}
+
+export default OrderStatusDetail;
